2f82a215e57ec67c880f68d58a0f98efb994d194
iRaziul <raziul.cse@gmail.com>
2023-09-14 15:15:14 +0600

    Using container class

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
         <!-- Page Heading -->
         @if (isset($header))
             <header class="bg-white shadow">
-                <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="container px-4 py-6 mx-auto sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
             </header>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="container px-4 mx-auto sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
